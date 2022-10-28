137ef9de89d8ab46e3acfe73e25d1afd6b4ffe2c
honginho <inoh0914@gmail.com>
2022-10-28 16:21:21 +0900

    fix : package.json 버전 수정
    Former-commit-id: 103a98e97b99171a7c3027cf6909098033fd3ee3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <router-view />
+    
     <router-link :to="{ name: 'home' }"> <div>홈</div></router-link>
     <router-link :to="{ name: 'select' }">
       <div>프로젝트 선택</div></router-link
     >
     <router-link :to="{ name: 'docs' }"> <div>공유 api 문서</div></router-link>
-    <router-link :to="{ name: 'test' }"> <div>api test</div></router-link>
+    <router-link :to="{ name: 'test' }"> <div>api test</div></router-link><router-view />
   </div>
 </template>
 
