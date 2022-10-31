ad22cfffd05230b3c68ea91916da6f8649ad277a
정승욱 <tmddnrdl333@naver.com>
2022-10-31 16:23:53 +0900

    feat: websocket refresh, focus 구현
    Former-commit-id: fcd0377e13c2fe6efb8614b7bb5b9951dc347033

--- frontend/src/views/WebSocket.vue
+++ frontend/src/views/WebSocket.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <input type="text" v-model="content" />
-    <button @click="sendfunc()">SEND</button>
-    {{ content }}
+    {{ userName }}
+    <button @click="refreshReq()">SEND</button>
+    <br />
+    <input type="text" v-model="focus.row" /> <br />
+    <input type="text" v-model="focus.col" /> <br />
+    <button @click="focusChange()">FOCUS</button>
   </div>
 </template>
 
@@ -15,26 +18,74 @@ import Stomp from "stompjs";
 export default {
   setup() {
     return {
-      content: ref(""),
       socket: null,
       stompClient: null,
+
+      project_id: ref("20221031001"), // 프로젝트 선택 구현 후 가져올 것.
+      userName: ref(null), // 로그인 구현 후 가져올 것.
+      focus: ref({ row: "r", col: "c" }),
     };
   },
   created() {
+    this.userName = Math.round(Math.random() * 1000);
+
     this.socket = new SockJS("http://localhost:8081/api/v1/ws");
     this.stompClient = Stomp.over(this.socket);
     this.stompClient.connect({}, () => {
-      this.stompClient.subscribe("/sub/1", (msg) => {
-        const res = JSON.parse(msg.body);
-        console.log(res);
-      });
+      this.stompClient.subscribe(
+        "/sub/" + this.project_id + "/refresh",
+        (msg) => {
+          var res = JSON.parse(msg.body);
+          // 내가 보낸 메세지라면 return
+          if (res.userName == this.userName) return;
+          // 다른 사람이 보낸 메세지라면 계속
+
+          // TODO: Refresh
+          console.log("다른 사용자가 REFRESH 요청을 보냈습니다.");
+          console.log(res.content);
+          // --
+        }
+      );
+      this.stompClient.subscribe(
+        "/sub/" + this.project_id + "/focus",
+        (msg) => {
+          var res = JSON.parse(msg.body);
+          // 내가 보낸 메세지라면 return
+          if (res.userName == this.userName) return;
+          // 다른 사람이 보낸 메세지라면 계속
+
+          // TODO: Focus
+          // 다른 사람의 포커스 위치를 옮겨줌. (내껀 내 프론트에서만 보여줌)
+          console.log(
+            res.userName,
+            "사용자가 FOCUS를 ",
+            res.content,
+            "로 변경하였습니다."
+          );
+          // --
+        }
+      );
     });
   },
   methods: {
-    sendfunc() {
-      console.log("hi");
-      const req = this.content;
-      this.stompClient.send("/pub/1", {}, JSON.stringify(req));
+    refreshReq() {
+      const req = { userName: this.userName, content: "refresh" };
+      this.stompClient.send(
+        "/pub/" + this.project_id + "/refresh",
+        {},
+        JSON.stringify(req)
+      );
+    },
+    focusChange() {
+      const req = {
+        userName: this.userName,
+        content: JSON.stringify(this.focus),
+      };
+      this.stompClient.send(
+        "/pub/" + this.project_id + "/focus",
+        {},
+        JSON.stringify(req)
+      );
     },
   },
 };
