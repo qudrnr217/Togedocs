8a3803253c429a3682261d836eb4067df68e01d9
김하연 <kng06325@gmail.com>
2022-11-01 13:15:46 +0900

    feat: create mongodb config
    Former-commit-id: eae9077bfd89e461f9de7430156a2385bcacc553

--- backend/src/main/java/com/togedocs/backend/domain/entity/Apidocs.java
+++ backend/src/main/java/com/togedocs/backend/domain/entity/Apidocs.java
@@ -1,6 +1,6 @@
 package com.togedocs.backend.domain.entity;
 
-import lombok.Getter;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,8 +8,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 import java.util.Map;
 
+// TODO 어노테이션 수정해야함.
 @Document(collection = "apidocs")
+@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Apidocs {
 
     @Id
--- backend/src/main/java/com/togedocs/backend/domain/entity/ColDto.java
+++ backend/src/main/java/com/togedocs/backend/domain/entity/ColDto.java
@@ -1,10 +1,14 @@
 package com.togedocs.backend.domain.entity;
 
-import lombok.Data;
+import lombok.*;
 
 import java.util.UUID;
 
-@Data
+// TODO 어노테이션 수정해야함.
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ColDto {
     private String uuid;
     private String name;
