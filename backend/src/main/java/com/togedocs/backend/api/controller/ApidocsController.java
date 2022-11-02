b9fb2399ca317dc90a99552ed639226fbb3989d5
정승욱 <tmddnrdl333@naver.com>
2022-11-02 10:51:56 +0900

    feat: apidocs controller 5~9
    Former-commit-id: a8c04a5b6c6d1252f6c5ac28f977fba5dde0adca

--- backend/src/main/java/com/togedocs/backend/api/controller/ApidocsController.java
+++ backend/src/main/java/com/togedocs/backend/api/controller/ApidocsController.java
@@ -16,42 +16,45 @@ public class ApidocsController {
     private ApidocsService apidocsService;
 
     @PostMapping("/{projectId}/rows")
-    public ResponseEntity<?> addRow(@PathVariable Long projectId){
+    public ResponseEntity<?> addRow(@PathVariable Long projectId) {
         return null;
     }
 
     @PostMapping("/{projectId}/cols")
-    public ResponseEntity<?> addCol(@PathVariable Long projectId, @RequestBody ApidocsRequest.AddColRequest request){
+    public ResponseEntity<?> addCol(@PathVariable Long projectId, @RequestBody ApidocsRequest.AddColRequest request) {
         return null;
     }
 
     @PatchMapping("/{projectId}/rows")
-    public ResponseEntity<?> moveRow(@PathVariable Long projectId, @RequestBody ApidocsRequest.MoveItemRequest request){
+    public ResponseEntity<?> moveRow(@PathVariable Long projectId, @RequestBody ApidocsRequest.MoveItemRequest request) {
         return null;
     }
 
     @PatchMapping("/{projectId}/cols")
-    public ResponseEntity<?> moveCol(@PathVariable Long projectId, @RequestBody ApidocsRequest.MoveItemRequest request){
+    public ResponseEntity<?> moveCol(@PathVariable Long projectId, @RequestBody ApidocsRequest.MoveItemRequest request) {
         return null;
     }
 
     @DeleteMapping("/{projectId}/rows/{rowId}")
-    public ResponseEntity<?> deleteRow(@PathVariable Long projectId, @PathVariable String rowId){
-        return null;
+    public ResponseEntity<ApidocsResponse.Ids> deleteRow(@PathVariable Long projectId, @PathVariable String rowId) {
+        ApidocsResponse.Ids response = apidocsService.deleteRow(projectId, rowId);
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{projectId}/cols/{colId}")
-    public ResponseEntity<?> deleteCol(@PathVariable Long projectId, @PathVariable String colId){
-        return null;
+    public ResponseEntity<?> deleteCol(@PathVariable Long projectId, @PathVariable String colId) {
+        ApidocsResponse.Ids response = apidocsService.deleteCol(projectId, colId);
+        return ResponseEntity.ok().body(response);
     }
 
     @PatchMapping("/{projectId}")
-    public ResponseEntity<?> updateCell(@PathVariable Long projectId, @RequestBody ApidocsRequest.UpdateCellRequest request){
-        return null;
+    public ResponseEntity<?> updateCell(@PathVariable Long projectId, @RequestBody ApidocsRequest.UpdateCellRequest request) {
+        ApidocsResponse.Ids response = apidocsService.updateCell(projectId, request);
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/{projectId}")
-    public ResponseEntity<ApidocsResponse.Apidocs> getDocs(@PathVariable Long projectId){
+    public ResponseEntity<ApidocsResponse.Apidocs> getDocs(@PathVariable Long projectId) {
         ApidocsResponse.Apidocs response = apidocsService.getDocs(projectId);
         return ResponseEntity.ok().body(response);
     }
