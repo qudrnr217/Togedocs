9b5ca9c950d10680b65701c66907688136963383
김하연 <kng06325@gmail.com>
2022-11-02 10:38:07 +0900

    feat: api 1~4
    Former-commit-id: 1ec46ff6526ebd5b14f088305e5d260ef331e288

--- backend/src/main/java/com/togedocs/backend/api/service/ApidocsService.java
+++ backend/src/main/java/com/togedocs/backend/api/service/ApidocsService.java
@@ -3,14 +3,16 @@ package com.togedocs.backend.api.service;
 import com.togedocs.backend.api.dto.ApidocsRequest;
 import com.togedocs.backend.api.dto.ApidocsResponse;
 import com.togedocs.backend.domain.entity.Apidocs;
+import com.togedocs.backend.domain.entity.ColDto;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.bson.Document;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 import java.util.List;
 
@@ -20,20 +22,61 @@ public class ApidocsService {
 
     private MongoTemplate mongoTemplate;
 
-    public void addRow(Long projectId) {
+    public ApidocsResponse.Ids addRow(Long projectId)  {
+        Query query = new Query().addCriteria(Criteria.where("projectId").is(projectId));
+        Update update = new Update();
+        String rowId = UUID.randomUUID().toString();
+        update.push("rows", rowId);
+        update.set("data." + rowId, new Document());
+        mongoTemplate.updateFirst(query, update, "apidocs");
 
+        return ApidocsResponse.Ids.build(projectId, rowId, null);
     }
 
-    public void addCol(Long projectId, ApidocsRequest.AddColRequest request) {
+    public ApidocsResponse.Ids addCol(Long projectId, ApidocsRequest.AddColRequest request)  {
+        Query query = new Query().addCriteria(Criteria.where("projectId").is(projectId));
+        Update update = new Update();
+        String colId = UUID.randomUUID().toString();
+        ColDto col = ColDto.build(colId, request.getName(), request.getType());
+        update.push("cols", col);
+        mongoTemplate.updateFirst(query, update, "apidocs");
 
+        return ApidocsResponse.Ids.build(projectId, null, colId);
     }
 
-    public void moveRow(Long projectId, ApidocsRequest.MoveItemRequest request) {
+    public ApidocsResponse.Ids moveRow(Long projectId, ApidocsRequest.MoveItemRequest request)  {
+        Query query = new Query().addCriteria(Criteria.where("projectId").is(projectId));
+        Update update = new Update();
+        // rowId를 배열에서 제거
+        update.pull("rows", request.getFromId());
+        mongoTemplate.updateFirst(query, update, "apidocs");
+        // rowId를 배열의 특정 위치에 추가
+        update = new Update();
+        update.push("rows").atPosition(request.getToIndex()).value(request.getFromId());
+        mongoTemplate.updateFirst(query, update, "apidocs");
 
+        return ApidocsResponse.Ids.build(projectId, request.getFromId(), null);
     }
 
-    public void moveCol(Long projectId, ApidocsRequest.MoveItemRequest request) {
+    public ApidocsResponse.Ids moveCol(Long projectId, ApidocsRequest.MoveItemRequest request) {
+        Query query = new Query().addCriteria(Criteria.where("projectId").is(projectId));
+        Update update = new Update();
+        // col을 배열에서 제거
+        update.pull("cols", Query.query(Criteria.where("uuid").is(request.getFromId())));
+        Apidocs apidocs = mongoTemplate.findAndModify(query, update, Apidocs.class);
+        List<ColDto> colDtos = apidocs.getCols();
+        ColDto targetCol = new ColDto();
+        for (ColDto colDto: colDtos) {
+            if(colDto.getUuid().equals(request.getFromId())){
+                targetCol = colDto;
+            }
+        }
+        update = new Update();
+        // col을 배열의 특정 위치에 추가
+        update.push("cols").atPosition(request.getToIndex()).value(targetCol);
+        mongoTemplate.updateFirst(query, update, "apidocs");
 
+        return ApidocsResponse.Ids.build(projectId, null, request.getFromId());
     }
 
     public ApidocsResponse.Ids deleteRow(Long projectId, String rowId) {
