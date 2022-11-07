0b6fcf1f7d9add15b667bece262c5fe18d30fe5d
qudrnr217 <dksk1233@naver.com>
2022-11-07 16:54:49 +0900

    feat: 소셜로그인 기능 구현
    Former-commit-id: 9e5d7ab5831f19ba9359ea03232b2484123d8b83

---
+++ backend/src/main/java/com/togedocs/backend/common/security/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.togedocs.backend.common.security.config;
+
+import com.togedocs.backend.common.security.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity //스프링시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+//                .antMatchers("/manager/**").access("hasRole('DVELOPER')")
+//                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("http://localhost:8080/")
+                .defaultSuccessUrl("myapp://")
+                .userInfoEndpoint().userService(principalOauth2UserService);
+        return http.build();
+    }
+
+    /*
+    기존: WebSecurityConfigurerAdapter를 상속하고 configure매소드를 오버라이딩하여 설정하는 방법
+    => 현재: SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+    //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+
+    @Override
+    protected void configure(HttpSecurity http) throws  Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin").access("\"hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+    }
+
+     */
+}
