4b27ab62dd2364137945e1e35e4016315d1c1b5b
DESKTOP-KVCQHCD\SSAFY <yi_jonghoon@naver.com>
2022-11-07 15:23:12 +0900

    feat: projectcard
    Former-commit-id: 720cd90fb475397b6c2a3b0e61cbc8df10ff7083

--- frontend/src/views/ProjectSelect.vue
+++ frontend/src/views/ProjectSelect.vue
@@ -1,146 +1,148 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-page-container>
-      <div class="project-list column items-center">
-        <div class="column">
-          <q-btn
-            stack
-            icon="add"
-            color="primary"
-            class="create-project-btn"
-            @click="createNewProjectBtnClicked = true"
-            ><q-tooltip class="bg-positive"
-              >새로운 프로젝트를 생성합니다</q-tooltip
-            ></q-btn
-          >
-        </div>
-        <div
-          class="col-4 project-item"
-          v-for="(project, idx) in projects"
-          :key="idx"
-        >
-          <project-card :projectItem="project"></project-card>
-        </div>
+  <div>
+    <div class="projectheader">
+      <div class="buttons">
+        <button>edit</button>
+        <button>로그아웃</button>
       </div>
-    </q-page-container>
-    <q-dialog v-model="createNewProjectBtnClicked">
-      <q-card class="createNewProjectDialog">
-        <q-card-section>
-          <div class="text-h6">새 프로젝트 생성</div>
-        </q-card-section>
-        <q-separator />
-        <q-card-section class="scroll">
-          <div class="q-gutter-y-md column">
-            <q-input
-              class="col"
-              clearable
-              filled
-              type="url"
-              v-model="newProject.title"
-              label="프로젝트 이름"
-            />
-            <q-input
-              class="col"
-              clearable
-              filled
-              v-model="newProject.url"
-              label="프로젝트 URL"
-            />
-            <q-input
-              class="col"
-              clearable
-              filled
-              autogrow
-              v-model="newProject.desc"
-              label="프로젝트 설명"
-            />
-            <div class="row justify-between">
-              <q-input
-                class="col-5"
-                clearable
-                filled
-                v-model="newProject.date.from"
-                mask="date"
-                :rules="['date']"
-                label="프로젝트 시작일"
-              >
-                <template v-slot:append>
-                  <q-icon name="event" class="cursor-pointer">
-                    <q-popup-proxy
-                      cover
-                      transition-show="scale"
-                      transition-hide="scale"
-                    >
-                      <q-date minimal v-model="newProject.date.from">
-                        <div class="row items-center justify-end">
-                          <q-btn
-                            v-close-popup
-                            label="Close"
-                            color="primary"
-                            flat
-                          />
-                        </div>
-                      </q-date>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
-              <q-icon name="arrow_right_alt" size="3.5rem" color="grey-6" />
-              <q-input
-                class="col-5"
-                clearable
-                filled
-                v-model="newProject.date.to"
-                mask="date"
-                :rules="['date']"
-                label="프로젝트 종료일"
-              >
-                <template v-slot:append>
-                  <q-icon name="event" class="cursor-pointer">
-                    <q-popup-proxy
-                      cover
-                      transition-show="scale"
-                      transition-hide="scale"
-                    >
-                      <q-date minimal v-model="newProject.date.to">
-                        <div class="row items-center justify-end">
-                          <q-btn
-                            v-close-popup
-                            label="Close"
-                            color="primary"
-                            flat
-                          />
-                        </div>
-                      </q-date>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
-            </div>
+      <div class="header">
+        <div class="profileimg">
+          <img src="@/assets/logo.png" alt="" />
+        </div>
+        <div class="username">{사용자이름}</div>
+      </div>
+
+      <div class="shadow"></div>
+    </div>
+
+    <q-layout view="hHh lpR fFf">
+      <q-page-container>
+        <div class="project-list">
+          <div class="column">
+            <q-btn
+              stack
+              icon="add"
+              color="secondary"
+              class="create-project-btn"
+              @click="createNewProjectBtnClicked = true"
+              ><q-tooltip class="bg-positive"
+                >새로운 프로젝트를 생성합니다</q-tooltip
+              ></q-btn
+            >
           </div>
-        </q-card-section>
+          <div v-for="(project, idx) in projects" :key="idx">
+            <project-card :projectItem="project"></project-card>
+          </div>
+        </div>
+      </q-page-container>
+      <q-dialog v-model="createNewProjectBtnClicked">
+        <q-card class="createNewProjectDialog">
+          <q-card-section>
+            <div class="text-h6">새 프로젝트 생성</div>
+          </q-card-section>
+          <q-separator />
+          <q-card-section class="scroll">
+            <div class="q-gutter-y-md column">
+              <q-input
+                class="col"
+                clearable
+                filled
+                type="url"
+                v-model="newProject.title"
+                label="프로젝트 이름" />
+              <q-input
+                class="col"
+                clearable
+                filled
+                v-model="newProject.url"
+                label="프로젝트 URL" />
+              <q-input
+                class="col"
+                clearable
+                filled
+                autogrow
+                v-model="newProject.desc"
+                label="프로젝트 설명" />
+              <div class="row justify-between">
+                <q-input
+                  class="col-5"
+                  clearable
+                  filled
+                  v-model="newProject.date.from"
+                  mask="date"
+                  :rules="['date']"
+                  label="프로젝트 시작일">
+                  <template v-slot:append>
+                    <q-icon name="event" class="cursor-pointer">
+                      <q-popup-proxy
+                        cover
+                        transition-show="scale"
+                        transition-hide="scale">
+                        <q-date minimal v-model="newProject.date.from">
+                          <div class="row items-center justify-end">
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat />
+                          </div>
+                        </q-date>
+                      </q-popup-proxy>
+                    </q-icon>
+                  </template>
+                </q-input>
+                <q-icon name="arrow_right_alt" size="3.5rem" color="grey-6" />
+                <q-input
+                  class="col-5"
+                  clearable
+                  filled
+                  v-model="newProject.date.to"
+                  mask="date"
+                  :rules="['date']"
+                  label="프로젝트 종료일">
+                  <template v-slot:append>
+                    <q-icon name="event" class="cursor-pointer">
+                      <q-popup-proxy
+                        cover
+                        transition-show="scale"
+                        transition-hide="scale">
+                        <q-date minimal v-model="newProject.date.to">
+                          <div class="row items-center justify-end">
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat />
+                          </div>
+                        </q-date>
+                      </q-popup-proxy>
+                    </q-icon>
+                  </template>
+                </q-input>
+              </div>
+            </div>
+          </q-card-section>
 
-        <q-separator />
+          <q-separator />
 
-        <q-card-actions align="right">
-          <q-btn
-            flat
-            label="취소"
-            color="primary"
-            v-close-popup
-            @click="resetCreateNewProjectDialog"
-          />
-          <q-btn
-            flat
-            label="생성"
-            color="primary"
-            v-close-popup
-            @click="createNewProject"
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </q-layout>
+          <q-card-actions align="right">
+            <q-btn
+              flat
+              label="취소"
+              color="primary"
+              v-close-popup
+              @click="resetCreateNewProjectDialog" />
+            <q-btn
+              flat
+              label="생성"
+              color="primary"
+              v-close-popup
+              @click="createNewProject" />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </q-layout>
+  </div>
 </template>
 
 <script>
@@ -193,7 +195,10 @@ export default {
   /* padding: 2rem; */
 }
 .project-list {
-  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
 }
 .create-project-btn {
   margin-right: 1rem;
@@ -208,5 +213,44 @@ export default {
 }
 .createNewProjectDialog {
   min-width: 40vw;
+}
+.profileimg {
+  display: flex;
+  justify-content: center;
+  width: 30vw;
+  border-radius: 50%;
+}
+.projectheader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+.shadow {
+  width: 100vw;
+  height: 5px;
+
+  background: linear-gradient(180deg, #e7e7e7 0%, rgba(231, 231, 231, 0) 100%);
+}
+.header {
+  display: flex;
+  width: 100vw;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vh;
+}
+.username {
+  font-size: 4vh;
+  font-weight: bold;
+  margin-bottom: 5vh;
+  margin-top: 5vh;
+}
+.buttons {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  top: 3vh;
+  right: 0;
 }
 </style>
