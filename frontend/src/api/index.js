5e93168407595e69207b8cd2d99f43bc5959bdd3
정승욱 <tmddnrdl333@naver.com>
2022-11-09 17:36:37 +0900

    feat: moveRow/Col시 focus 자동 이동, editing_content 유지
    Former-commit-id: 1edb69c027a95d1b96bd4e20f01f17a9ea1b475e

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import userStore from "@/store/modules/userStore";
-const BASEURL = "http://localhost:8081/api";
+// const BASEURL = "http://localhost:8081/api";
+const BASEURL = "http://k7a404.p.ssafy.io:8081/api";
 
 const api = axios.create({
   baseURL: BASEURL,
