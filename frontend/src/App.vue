22c07c8c8cc0e5afd5d4dd1b6a3cf9df23875831
김하연 <kng06325@gmail.com>
2022-11-10 13:50:36 +0900

    feat: add scroll area
    Former-commit-id: ffef28348b06a9d52483be43e2ff96ab6e1a7217

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,30 +20,4 @@ a {
   text-decoration: none;
   color: inherit;
 }
-
-::-webkit-scrollbar {
-  height: 8px;
-  width: 8px;
-  background: gray;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #888;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-
-::-webkit-scrollbar-thumb:horizontal {
-  background: rgb(150, 150, 150);
-  border-radius: 10px;
-}
 </style>
