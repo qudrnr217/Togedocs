bb4151ec1747caa174dbf5b10911f5e434a0ab79
honginho <inoh0914@gmail.com>
2022-11-15 14:31:19 +0900

    feat: vuex and ApiList Top
    Former-commit-id: bfaaf63d71634ce8948cbf149ca47cdaedb087e4

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,9 +6,21 @@ import projectStore from "@/store/modules/projectStore.js";
 export default createStore({
   state: {
     apiStoreList: [],
+    userId: "",
+    projectId: "",
   },
-  getters: {},
-  mutations: {},
+  getters: {
+    userId: (state) => state.userId,
+    projectId: (state) => state.projectId,
+  },
+  mutations: {
+    SET_USERID: (state, userId) => {
+      state.userId = userId;
+    },
+    SET_PROJECTID: (state, projectId) => {
+      state.projectId = projectId;
+    },
+  },
   actions: {},
   modules: {
     projectStore,
