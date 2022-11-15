279b397d16efd163788f712fcca509f365da1baf
정승욱 <tmddnrdl333@naver.com>
2022-11-15 15:58:41 +0900

    feat: addLog bug fixed
    Former-commit-id: 4c06695f2a70e922202411995626e0860f8bbe85

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,15 +7,20 @@ export default createStore({
   state: {
     apiStoreList: [],
     userId: 0,
+    userName: "",
     projectId: 0,
   },
   getters: {
     userId: (state) => state.userId,
+    userName: (state) => state.userName,
     projectId: (state) => state.projectId,
   },
   mutations: {
     SET_USERID: (state, userId) => {
       state.userId = userId;
+    },
+    SET_USERNAME: (state, userName) => {
+      state.userName = userName;
     },
     SET_PROJECTID: (state, projectId) => {
       state.projectId = projectId;
