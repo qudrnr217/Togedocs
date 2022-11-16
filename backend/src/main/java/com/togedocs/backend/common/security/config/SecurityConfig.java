fb5361bb1c06d1c37a635bb25694260751615a17
qudrnr217 <dksk1233@naver.com>
2022-11-16 15:28:57 +0900

    feat: test
    Former-commit-id: 4c3b55e4196fa705f842c2c92869e1185cd673db

--- backend/src/main/java/com/togedocs/backend/common/security/config/SecurityConfig.java
+++ backend/src/main/java/com/togedocs/backend/common/security/config/SecurityConfig.java
@@ -17,9 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
