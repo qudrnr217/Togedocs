73be3cf77c554dde551c2eeb554bc03cb3ed8b4c
정승욱 <tmddnrdl333@naver.com>
2022-11-16 16:29:55 +0900

    fix: url bugfix
    Former-commit-id: 363179f1097df62eb1ec837c2ffe2ddcb71d4a96

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import userStore from "@/store/modules/userStore";
 // const BASEURL = "http://localhost:8081";
-const BASEURL = "http://k7a404.p.ssafy.io:8081/api";
+const BASEURL = "http://k7a404.p.ssafy.io:8081";
 
 const api = axios.create({
   baseURL: BASEURL + "/api",
